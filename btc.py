0dad463363c71e549556b3ad6370f3f585fe1d6c
root <root@ip-172-31-53-163.ec2.internal>
2017-10-08 16:47:00 +0000

    does work... rewrite in progress

--- btc.py
+++ btc.py
@@ -1,22 +1,24 @@
-from auth import trading_client
 import datetime
 import wsclient
 import logging
+import redis
 import time
+import sys
 import os
 
-start_money = 100 # EUR
-commision = 0.0025 # 0.25%
-adjustment = 0.00001 # smallest value on b1tstamp?
+parameters  = {'start_money_value': 1,
+	       'commision': 0.0025,  # 0.25%
+	       'adjustment': 0.00001, } # smallest value on b1tstamp?
 
-success_eur = 0
-success_eur_c = 0
-success_usd = 0
-success_usd_c = 0
-highest_ratio_eur = 0
-highest_ratio_eur_c = 0
-highest_ratio_usd = 0
-highest_ratio_usd_c = 0
+conversions = {'btc': {'usd', 'eur'}, 
+               'eur': {'usd'}}
+
+directions  = {'buy': {'eur': {'btc', 'usd'}, 
+                       'usd': {'btc'}},
+	       'sell': {'btc': {'eur', 'usd'},
+                        'eur': {'usd'}}}
+
+counters = {}
 
 logger = logging.getLogger()
 for handler in logger.handlers:
@@ -25,113 +27,91 @@ loggerHandler = logging.FileHandler('/tmp/bitstamp-' + os.path.basename(__file__
 logger.addHandler(loggerHandler)
 logger.setLevel(logging.WARNING) # TODO: This doesn't work
 
-client = wsclient.BitstampWebsocketClient()
-time.sleep(1) # lame hack - we can subscribe only after connection is established
-client.subscribe("order_book", "btc", "usd")
-client.subscribe("order_book", "btc", "eur")
-client.subscribe("order_book", "eur", "usd")
+client_bitstamp_ws = wsclient.BitstampWebsocketClient()
+client_redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-def fetchPrice():
-	self = {}
-        self['btcusd_ask_v'] = float(client.orderbook['btc']['usd']['asks'][0][0])
-        self['btcusd_bid_v'] = float(client.orderbook['btc']['usd']['bids'][0][0])
-        self['btceur_ask_v'] = float(client.orderbook['btc']['eur']['asks'][0][0])
-        self['btceur_bid_v'] = float(client.orderbook['btc']['eur']['bids'][0][0])
-        self['eurusd_ask_v'] = float(client.orderbook['eur']['usd']['asks'][0][0])
-        self['eurusd_bid_v'] = float(client.orderbook['eur']['usd']['bids'][0][0])
+def fetchOrderBook():
+        self = {}
+
+	for currency in conversions:
+		for conversion in conversions[currency]:
+			checkOrderBook(currency, conversion)
+			self[currency + conversion + '_ask_v'] = float(client_bitstamp_ws.orderbook[currency][conversion]['asks'][0][0])
+			self[currency + conversion + '_bid_v'] = float(client_bitstamp_ws.orderbook[currency][conversion]['bids'][0][0])
 
         return self
 
-def eur2btc(start_money, price):
-	global success_eur
-	global highest_ratio_eur
-	adjustment = 'false'
+def checkOrderBook(currency, conversion):
+        while ('asks' and 'bids') not in client_bitstamp_ws.orderbook[currency][conversion]:
+		print "Empty orderbook. Resubscribing.." + currency + " : " + conversion
+                subscribe(currency, conversion)
+                time.sleep(1)
+
+def subscribe(currency, conversion):
+        client_bitstamp_ws.subscribe("order_book", currency, conversion)
+
+def eur2btc(start_money, order_book, adjustment):
+	global counters
+	notice = ''
+
+	if adjustment != 'false':
+		notice = 'ADJUSTED'
 
 	self = {}
-	self['btc'] = buy(price, 'eur', 'btc', commision, adjustment, start_money) # buy btc with eur
-	self['usd'] = sell(price, 'btc', 'usd', commision, adjustment, self['btc']) # sell btc for usd
-	self['eur'] = buy(price, 'usd', 'eur', commision, adjustment, self['usd']) # buy eur with usd
+	self['btc'] = buy(order_book, 'eur', 'btc', adjustment, start_money) # buy btc with eur
+	self['usd'] = sell(order_book, 'btc', 'usd', adjustment, self['btc']) # sell btc for usd
+	self['eur'] = buy(order_book, 'usd', 'eur', adjustment, self['usd']) # buy eur with usd
 
-	success_eur       = increaseValue(start_money, self['eur'], success_eur)
-	ratio_eur         = self['eur'] / start_money
-	highest_ratio_eur = compare_and_update(highest_ratio_eur, ratio_eur)
+	ratio_eur = self['eur'] / start_money
+	if ratio_eur > 1:
+		executeBuy(start_money, self)
+
+	counters['highest_ratio_eur'] = compare_and_update(counters['highest_ratio_eur'], ratio_eur)
+	counters['success_eur'] = increaseValue(start_money, self['eur'], counters['success_eur'])
+
+	if adjustment != 'false':
+		notice = 'ADJUSTED'
 
 	conversionPrinter("EUR", start_money,
 			  "BTC", self['btc'],
 			  "USD", self['usd'],
 			  "EUR", self['eur'],
-			  ratio_eur, highest_ratio_eur, success_eur)
+			  ratio_eur, highest_ratio_eur, success_eur, notice)
 
-
-def eur2btc_c(start_money, price):
-	global success_eur_c
-	global highest_ratio_eur_c
-
-	self = {}
-        self['btc_c'] = buy(price, 'eur', 'btc', commision, adjustment, start_money) # buy btc with eur
-        self['usd_c'] = sell(price, 'btc', 'usd', commision, adjustment, self['btc_c']) # sell btc for usd
-        self['eur_c'] = buy(price, 'usd', 'eur', commision, adjustment, self['usd_c']) # buy eur with usd
-
-	success_eur_c       = increaseValue(start_money, self['eur_c'], success_eur_c)
-	ratio_eur_c         = self['eur_c'] / start_money
-	highest_ratio_eur_c = compare_and_update(highest_ratio_eur_c, ratio_eur_c)
-
-
-        conversionPrinter("EUR", start_money,
-                          "BTC", self['btc_c'],
-                          "USD", self['usd_c'],
-                          "EUR", self['eur_c'],
-                          ratio_eur_c, highest_ratio_eur_c, success_eur_c)
-
-def eur2usd(start_money, price):
+def eur2usd(start_money, order_book, adjustment):
 	global success_usd
         global highest_ratio_usd
-	adjustment = 'false'
+	notice = ''
 
 	self = {}
-	self['usd'] = sell(price, 'eur', 'usd', commision, adjustment, start_money) # sell eur for usd
-	self['btc'] = buy(price, 'usd', 'btc', commision, adjustment, self['usd']) # buy btc with usd
-	self['eur'] = sell(price, 'btc', 'eur', commision, adjustment, self['btc']) # sell btc for eur
+	self['usd'] = sell(order_book, 'eur', 'usd', commision, adjustment, start_money) # sell eur for usd
+	self['btc'] = buy(order_book, 'usd', 'btc', commision, adjustment, self['usd']) # buy btc with usd
+	self['eur'] = sell(order_book, 'btc', 'eur', commision, adjustment, self['btc']) # sell btc for eur
 
-	success_usd       = increaseValue(start_money, self['eur'], success_usd)
-	ratio_usd         = self['eur'] / start_money
-	highest_ratio_usd = compare_and_update(highest_ratio_usd, ratio_usd)
+	ratio_usd = self['eur'] / start_money
+	if ratio_usd > 1:
+                executeBuy(start_money, self)
+
+	counters['highest_ratio_usd'] = compare_and_update(highest_ratio_usd, ratio_usd)
+	counters['success_usd'] = increaseValue(start_money, self['eur'], success_usd)
+
+        if adjustment != 'false':
+                notice = 'ADJUSTED'
 
         conversionPrinter("EUR", start_money,
                           "USD", self['usd'],
                           "BTC", self['btc'],
                           "EUR", self['eur'],
-                          ratio_usd, highest_ratio_usd, success_usd)
+                          ratio_usd, highest_ratio_usd, success_usd, notice)
 
-def eur2usd_c(start_money, price):
-	global success_usd_c
-	global highest_ratio_usd_c
-
-        self = {}
-        self['usd_c'] = sell(price, 'eur', 'usd', commision, adjustment, start_money) # sell eur for usd
-        self['btc_c'] = buy(price, 'usd', 'btc', commision, adjustment, self['usd_c']) # buy btc with usd
-        self['eur_c'] = sell(price, 'btc', 'eur', commision, adjustment, self['btc_c']) # sell btc for eur
-
-	success_usd_c       = increaseValue(start_money, self['eur_c'], success_usd_c)
-	ratio_usd_c         = self['eur_c'] / start_money
-	highest_ratio_usd_c = compare_and_update(highest_ratio_usd_c, ratio_usd_c)
-
-        conversionPrinter("EUR", start_money,
-                          "USD", self['usd_c'],
-                          "BTC", self['btc_c'],
-                          "EUR", self['eur_c'],
-                          ratio_usd_c, highest_ratio_usd_c, success_usd_c)
-
-def conversionPrinter(startCur, start_money, firstX, firstV, secondX, secondV, thirdX, thirdV, ratio, ratioH, success):
-	output = (startCur,": ",start_money,
-		    firstX,": ",round(firstV, 5),
-		   secondX,": ",round(secondV, 5),
-		    thirdX,": ",round(thirdV, 5),
-		     "(ratio: ",ratio,
-		", max ratio: ",ratioH,
-		  ", success: ",success)
-
-	print output
+def conversionPrinter(startCur, start_money, firstX, firstV, secondX, secondV, thirdX, thirdV, ratio, ratioH, success, notice):
+	print (startCur,": ",start_money,
+	         firstX,": ",round(firstV, 5),
+	        secondX,": ",round(secondV, 5),
+		 thirdX,": ",round(thirdV, 5),
+		  "(ratio: ",ratio,
+	     ", max ratio: ",ratioH,
+	       ", success: ",success,notice)
 
 def increaseValue(first, second, third):
 	self = third
@@ -148,43 +128,44 @@ def compare_and_update(first, second):
 
         return self
 
-def buy(orderbook, fromCurrency, toCurrency, commision, adjustment, amount):
+def buy(orderbook, fromCurrency, toCurrency, adjustment, amount):
+	global parameters
+
 	if adjustment == 'false':
-		self = (amount / orderbook[toCurrency + fromCurrency + '_ask_v']) - ((amount / orderbook[toCurrency + fromCurrency + '_ask_v']) * commision)
+		self = ((amount / orderbook[toCurrency + fromCurrency + '_ask_v']) - 
+		       ((amount / orderbook[toCurrency + fromCurrency + '_ask_v']) * parameters['commision']))
 		return self
 
-	self = ((amount / orderbook[toCurrency + fromCurrency + '_bid_v']) + adjustment) - ((amount / (orderbook[toCurrency + fromCurrency + '_bid_v']) + adjustment) * commision)
+	self = ((amount / (orderbook[toCurrency + fromCurrency + '_bid_v']) + adjustment) - 
+	       ((amount / (orderbook[toCurrency + fromCurrency + '_bid_v']) + adjustment) * parameters['commision']))
+
 	return self
 
-def sell(orderbook, fromCurrency, toCurrency, commision, adjustment, amount):
+def sell(orderbook, fromCurrency, toCurrency, adjustment, amount):
 	if adjustment == 'false':
-		self = (amount * orderbook[fromCurrency + toCurrency + '_bid_v']) - ((amount * orderbook[fromCurrency + toCurrency + '_bid_v']) * commision)
+		self = ((amount * orderbook[fromCurrency + toCurrency + '_bid_v']) - 
+		       ((amount * orderbook[fromCurrency + toCurrency + '_bid_v']) * parameters['commision']))
 		return self
 
-	self = (amount * (orderbook[fromCurrency + toCurrency + '_ask_v']) - adjustment) - ((amount * (orderbook[fromCurrency + toCurrency + '_ask_v']) - adjustment) * commision)
+	self = ((amount * (orderbook[fromCurrency + toCurrency + '_ask_v']) - adjustment) - 
+	       ((amount * (orderbook[fromCurrency + toCurrency + '_ask_v']) - adjustment) * parameters['commision']))
+
 	return self
 
-def doStuff(start_money, price):
-	eur2btc(start_money, price)
-	eur2btc_c(start_money, price)
-	eur2usd(start_money, price)
-	eur2usd_c(start_money, price)
+def doStuff(order_book):
+	global parameters
+	global directions
 
+	eur2btc(parameters['start_money_value'], order_book, 'false')
+	eur2btc(parameters['start_money_value'], order_book, parameters['adjustment'])
+	eur2usd(parameters['start_money_value'], order_book, 'false')
+	eur2usd(parameters['start_money_value'], order_book, parameters['adjustment'])
 
 # ------ START HERE
 
-while ('asks' and 'bids') not in client.orderbook['btc']['usd']:
-	time.sleep(0.1)
-while ('asks' and 'bids') not in client.orderbook['btc']['eur']:
-	time.sleep(0.1)
-while ('asks' and 'bids') not in client.orderbook['eur']['usd']:
-	time.sleep(0.1)
-
-print(trading_client.account_balance()['fee'])
-
 while True:
-	price = fetchPrice()
-	if price != fetchPrice():
+	order_book = fetchOrderBook()
+	if order_book != fetchOrderBook():
 		print "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 		print datetime.datetime.now()
-#		doStuff(start_money, price)
+		doStuff(order_book)
